ed0a1f5be566ed5ff8d0853dc294e555b577370a
Shaliamekh <62338990+Shaliamekh@users.noreply.github.com>
2020-08-14 14:05:53 +0800

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^$', test),
     url(r'^login/', test),
     url(r'^signup/', test),
-    url(r'^question/\d+/$', test),
+    url(r'^question/(?P<pk>\d+)/', test),
     url(r'^ask/', test),
     url(r'^popular/', test),
     url(r'^new/', test),
